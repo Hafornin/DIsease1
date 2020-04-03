3c7b8120367ea0d824c3bc5c2b91fa8edfc7dbe7
Aurore Wendling <aurore.wendling34@gmail.com>
2020-04-03 15:57:22 +0200

    new folder + new individual class

--- Simulation.java
+++ Simulation.java
@@ -3,7 +3,7 @@ public class Simulation {
 	public static void main (String[] args) {
 		System.out.println("Coucou");
 		Disease d = new Disease(0.01,14,14);
-		Group g = new Group(500000,60,d);
+		Group g = new Group(500000,60,d,5,5);
 		g.group.get(0).infectedTime = 1;
 		g.group.get(0).updateState();
 		for(int i=0;i<60;i++){
