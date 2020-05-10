9c71e2f19087c97fdf9989216d675ef30e2eb254
Aurore Wendling <aurore.wendling34@gmail.com>
2020-05-10 11:17:52 +0200

    mlk

--- Disease_model/Disease_model/src/disease/TestMain.java
+++ Disease_model/Disease_model/src/disease/TestMain.java
@@ -7,6 +7,6 @@ public class TestMain {
 		Disease d = new Disease(60, 100, 10, 0.1, 0, 0, 50);
 		SimulationPanel nineg = new MultipleGroupsPanel(d,8);    
 		SimulationPanel oneg = new OneGroupPanel(d); 
-		Simulation s = new Simulation(nineg,10);
+		Simulation s = new Simulation(oneg,10);
 	}
 }
